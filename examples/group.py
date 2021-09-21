19188a53009910f3f93a5615531343700f883e07
Sengolda <senushkabatawala@gmail.com>
2021-09-21 10:33:45 +0530

    Adding examples

---
+++ examples/group.py
@@ -0,0 +1,14 @@
+from cli.cli import CLI
+
+my_cli = CLI("My Nice CLI tool!")
+
+@my_cli.group(name="hi")
+async def hi():
+    print("Hello World!")
+
+@hi.command(name="you")
+def you():
+    print("Hi you, yes you are cool!")
+
+
+my_cli.run()
---
+++ examples/no_welcome_messages.py
@@ -0,0 +1,9 @@
+from cli.cli import CLI
+
+my_cli = CLI("My Nice CLI tool with no welcome messages!", no_welcome_message=True)
+
+@my_cli.command(name="hi")
+def hi():
+    print("Hello World!")
+
+my_cli.run()

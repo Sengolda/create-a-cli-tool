ea240654ea2e0fb3ce92afd095dcdc42634c5a6b
Lint Action <lint-action@samuelmeuli.com>
2021-09-21 04:49:29 +0000

    Fix code style issues with Black

--- cli/cli.py
+++ cli/cli.py
@@ -1,6 +1,5 @@
-from .commands import Command,CommandGroup as Group
+from .commands import Command, CommandGroup as Group
 import inspect
-
 
 
 class CLI:
@@ -19,7 +18,7 @@ class CLI:
         def decorator(func):
             if inspect.iscoroutinefunction(func):
                 raise RuntimeError("Functions must not be coroutines.")
-        
+
             if not name:
                 cmd = Command.from_function(func)
             else:
@@ -32,20 +31,20 @@ class CLI:
             return cmd
 
         return decorator
-    
+
     def group(self, name: str = None):
         def decorator(func):
             if inspect.iscoroutinefunction(func):
                 raise RuntimeError("Functions must not be coroutines.")
-            
+
             if not name:
                 cmd = Group.from_function(func)
             else:
                 cmd = Group(name=name, func=func)
             self.commands.append(cmd)
             return cmd
-        return decorator
 
+        return decorator
 
     def run(self):
         if not self.no_welcome_message:
@@ -57,11 +56,11 @@ class CLI:
             if not cmd:
                 print(self.command_not_found_message)
                 return
-            
+
             elif isinstance(cmd, Command) and len(args) == 1:
                 cmd._func()
                 break
-            
+
             elif len(args) == 2:
                 for subcmd in cmd:
                     if subcmd.name == args[1]:
@@ -73,7 +72,6 @@ class CLI:
         for command in self.commands:
             if command.name == name:
                 return command
-    
 
     def remove_command(self, name: str):
         for cmd in self.commands:
--- cli/commands.py
+++ cli/commands.py
@@ -12,12 +12,9 @@ class CommandGroup(Command):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.children = []
-    
 
     def __iter__(self):
         return iter(self.children)
-
-
 
     def command(self, name: str = None):
         def decorator(func):
@@ -25,11 +22,12 @@ class CommandGroup(Command):
                 command = Command.from_function(func)
             else:
                 command = Command(name=name, func=func)
-                
+
             if command.name.count(" ") > 0:
                 raise RuntimeError("Command names cannot have spaces.")
             self.children.append(command)
             return command
+
         return decorator
 
     def get_subcommand(self, name: str):

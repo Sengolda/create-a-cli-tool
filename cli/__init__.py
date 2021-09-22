927ac78727f0df0e454fec2be79411d6fb8b6623
Sengolda <senushkabatawala@gmail.com>
2021-09-22 22:46:46 +0530

    Stable releasing on pypi (0.2.0)

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,7 +1,7 @@
 from .cli import *
 from .commands import *
 
-__version__ = "0.2.0a"
+__version__ = "0.2.0"
 __author__ = "Sengolda"
 __copyright__ = "Copyright (c) 2021-present Sengolda"
 __license__ = "MIT"

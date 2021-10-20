74e7da6445f5a32a29e89b95f7d755aac2427674
Sengolda <senushkabatawala@gmail.com>
2021-10-20 15:25:45 +0530

    Bumped to 0.3.4

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,7 +1,7 @@
 from .cli import *
 from .commands import *
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __author__ = "Sengolda"
 __copyright__ = "Copyright (c) 2021 Sengolda"
 __license__ = "MIT"

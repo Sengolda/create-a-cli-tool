37304565fa7cc3425161b68087c999945300eb47
Sengolda <senushkabatawala@gmail.com>
2023-05-17 13:49:59 +0530

    Version bump and release to pypi

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,7 +1,7 @@
 from .cli import *
 from .commands import *
 
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 __author__ = "Sengolda"
 __copyright__ = "Copyright (c) 2021 Sengolda"
 __license__ = "MIT"
